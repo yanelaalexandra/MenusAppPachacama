163decde3cc9dbb363412c5fb8f0683acefda652
yanelaalexandra <yanela.pachacama@tecsup.edu.pe>
2018-04-12 14:36:09 -0500

    Segundo commit en clase

--- app/src/main/java/com/pachacama/menus/menusapppachacama/DummyActivity.java
+++ app/src/main/java/com/pachacama/menus/menusapppachacama/DummyActivity.java
@@ -1,7 +1,9 @@
 package com.pachacama.menus.menusapppachacama;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DummyActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class DummyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy);
+    }
+
+    public void menutoolbar(View view){
+        startActivity(new Intent(this, MenuToobarActivity.class));
     }
 }
